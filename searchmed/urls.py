9b28875764d17cd3d78d24144a4e9ba0712c3cd9
maroo1746 <seoyeong4700@gmail.com>
2023-10-02 14:25:28 +0900

    link home page and combine page

--- searchmed/urls.py
+++ searchmed/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-
+app_name = 'searchmed'
 
 urlpatterns = [
 path('', views.home, name='home'),
+
+path('combine/', views.combine, name='combine'),
 ]
--- searchmed/views.py
+++ searchmed/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request) :
 	return render(request, 'searchmed/home.html', {})
+
+def combine(request) :
+    return render(request, 'searchmed/combine.html', {})
