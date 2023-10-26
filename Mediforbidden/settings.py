cb350479039fa22b90cdabfddc9927bc9a11e1e3
maroo1746 <seoyeong4700@gmail.com>
2023-10-27 06:50:33 +0900

    change the medicine display list to table in validation part (medicine will be displayed to user by table, not list)

--- Mediforbidden/settings.py
+++ Mediforbidden/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import my_settings
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
