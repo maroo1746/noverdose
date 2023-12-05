b125a9c5f90e80adf43376eff0405b63087428dc
bibejackson <bibejackson@korea.ac.kr>
2023-12-05 20:07:27 +0900

    edit .env encrypt pgp

--- my_settings.py
+++ my_settings.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE' : 'django.db.backends.mysql',
         'NAME' : 'med_db',
         'USER' : 'root',
-        'PASSWORD' : 'snewi832#',
+        'PASSWORD' : os.environ.get('DB_PASSWORD'),
         #'HOST': os.getenv('DB_HOST', 'localhost'),
         'HOST' : 'db',
         #'HOST' : 'localhost',
@@ -17,4 +17,4 @@ DATABASES = {
 }
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!q7#u-1tucuqadzf7(z6i&ql#qam$=2x(b4p6%3m2fv%j6&cvq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
